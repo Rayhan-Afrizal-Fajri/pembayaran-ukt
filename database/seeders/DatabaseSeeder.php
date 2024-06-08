02677566455f3837dab60cadefd5209bf21b48a3
Blasty45 <mataramkuno1945@gmail.com>
2024-06-08 23:19:45 +0700

    Penambahan beberapa file & menyelesaikan login & register

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // User::factory(10)->withPersonalTeam()->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory()->withPersonalTeam()->create(
+            [
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('admin123'),
+            'isAdmin' => true
+        ]
+        );
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,34 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Livewire\Profile\DetailUserInformation;
+use App\Livewire\Users\DaftarPembayaran;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+// masih ada kekurangan terkait middleware login
+
+Route::get('/daftar-mahasiswa', [AdminController::class , "index"])->name('daftar-mahasiswa');
+
+Route::get('/pembayaran-mahasiswa', function () {
+    return view('pembayaran-mahasiswa');
+})->name('pembayaran-mahasiswa');
+
+// Route::get('/daftar-pembayaran', function () {
+//     return view('livewire.users.daftar-pembayaran');
+// })->name('daftar');
+
+Route::get('/daftar-pembayaran', DaftarPembayaran::class)->name('daftar-pembayaran');
